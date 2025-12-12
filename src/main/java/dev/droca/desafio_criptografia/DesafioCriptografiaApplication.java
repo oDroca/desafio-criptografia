ca91f3e1c9c7693c2b60b135bc4bee9dd67f8f8a
oDroca <davirochatz@gmail.com>
2025-12-12 16:13:16 -0300

    Inicio do projeto

---
+++ src/main/java/dev/droca/desafio_criptografia/DesafioCriptografiaApplication.java
@@ -0,0 +1,13 @@
+package dev.droca.desafio_criptografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCriptografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCriptografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/droca/desafio_criptografia/DesafioCriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.droca.desafio_criptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
